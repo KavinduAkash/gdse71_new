878750bf651425b96139ab0c0c4e8c42ff2edcdf
Kavindu Akash <bkkasamarasingha1220@gmail.com>
2025-04-24 15:02:23 +0530

    update event

---
+++ mini-lms/controller/StudentController.js
@@ -0,0 +1,61 @@
+import {students_db} from "../db/db.js";
+
+// load student records
+function loadStudents() {
+    $('#student-tbody').empty();
+    students_db.map((item, index) => {
+        let fname = item.fname;
+        let lname = item.lname;
+        let address = item.address;
+
+        let data = `<tr>
+                            <td>${index + 1}</td>
+                            <td>${fname}</td>
+                            <td>${lname}</td>
+                            <td>${address}</td>
+                        </tr>`
+
+        $('#student-tbody').append(data);
+    })
+}
+
+// save
+$('#student_save').on('click', function(){
+    // let fname = document.getElementById('fname').value;
+    let fname = $('#fname').val();
+    let lname = $('#lname').val();
+    let address = $('#address').val();
+
+    if(fname === '' || lname === '' || address === '') {
+        // alert("Invalid inputs!");
+
+        Swal.fire({
+            title: 'Error!',
+            text: 'Invalid Inputs',
+            icon: 'error',
+            confirmButtonText: 'Ok'
+        })
+    } else {
+
+
+        let student_data = {
+            fname: fname,
+            lname: lname,
+            address: address
+        };
+
+        students_db.push(student_data);
+
+        console.log(students_db);
+
+        loadStudents();
+
+        // .push(), .pop(), .shift(), unshift()
+
+        Swal.fire({
+            title: "Added Successfully!",
+            icon: "success",
+            draggable: true
+        });
+    }
+});
